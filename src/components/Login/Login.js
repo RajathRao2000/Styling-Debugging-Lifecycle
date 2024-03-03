dcfe733c2e21251db6a9dfaa7264a883bb25fb93
Rajath Rao <raorajath1998@gmail.com>
2024-03-04 00:00:45 +0530

    Rules of Hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
+import Input from "../UI/Input/Input";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -14,22 +16,17 @@ const emailReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 
-const passwordReducer = (state,action) =>{
+const passwordReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
-    console.log("in user ip")
-
     return { value: action.val, isValid: action.val.trim().length > 6 };
   }
   if (action.type === "INPUT_BLUR") {
-    console.log("in blur")
-
     return { value: state.value, isValid: state.value.trim().length > 6 };
   }
   return { value: "", isValid: false };
-}
+};
 
 const Login = (props) => {
-
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -42,6 +39,8 @@ const Login = (props) => {
     isValid: null,
   });
 
+  const authCtx = useContext(AuthContext);
+
   useEffect(() => {
     console.log("EFFECT RUNNING");
 
@@ -49,6 +48,9 @@ const Login = (props) => {
       console.log("EFFECT CLEANUP");
     };
   }, []);
+
+  const {isValid: emailIsValid}=emailState
+  const {isValid: passwordIsValid}=passwordState
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
@@ -59,7 +61,6 @@ const Login = (props) => {
   };
 
   const passwordChangeHandler = (event) => {
-    console.log("||",event.target.value)
     dispatchPassword({ type: "USER_INPUT", val: event.target.value });
 
     setFormIsValid(
@@ -72,46 +73,35 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    console.log("in validatePasswordHandler")
     dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+          id="email"
+          label="E-mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <Input
+          id="password"
+          label="Password"
+          type="password"
+          isValid={passwordIsValid}
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
